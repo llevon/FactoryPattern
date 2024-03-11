72cb0116e889d568143e1b721990883acaf4a34b
levon <levonkhachatrian7@gmail.com>
2024-03-11 13:32:05 +0400

    main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,23 @@
 package org.example
 
 fun main() {
-    println("Hello World!")
+    println(Computer.factory("Razer"))
 }
+
+interface Computer {
+    companion object {
+        fun factory(name: String): Computer {
+            return when (name) {
+                "Asus" -> Asus()
+                "Lenovo" -> Lenovo()
+                "Razer" -> Razer()
+                else -> throw Exception()
+            }
+
+        }
+    }
+}
+
+class Asus : Computer
+class Razer : Computer
+class Lenovo : Computer
